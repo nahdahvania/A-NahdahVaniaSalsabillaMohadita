a2d2201f951a815545411568ba54b8ee9a2c855e
nahdahvania <nahdah.vania17@gmail.com>
2025-01-10 13:48:13 +0700

    new

--- array/script.js
+++ array/script.js
@@ -2,8 +2,8 @@ let antrian = ["Ray", "Fiki", "Fadhilla", "Farah"];
 
 antrian.push("Nabila");
 antrian.push("Maza", "Elsi");
-antrian.pop(); // Removes "Elsi"
-antrian.shift(); // Removes "Ray"
-antrian.shift(); // Removes "Fiki"
+antrian.pop(); 
+antrian.shift(); 
+antrian.shift(); 
 antrian.unshift("Tomi");
 console.log("Hasil akhir antrian:", antrian);
